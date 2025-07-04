80c684d35fd361cdfa08d9036a21a41ff92258e7
m565656m <mkomko624567@gmail.com>
2025-07-04 03:04:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,26 +2,38 @@ from flask import Flask, request
 import requests
 import os
 
-TOKEN = "7880550955:AAEep2yo54KzCLXqKHUWcTOTIODQbZsck_4"
-CHAT_ID = "6969597735"
+# قراءة التوكن والشات آي دي من متغيرات البيئة
+7880550955:AAEep2yo54KzCLXqKHUWcTOTIODQbZsck_4 = os.environ.get("7880550955:AAEep2yo54KzCLXqKHUWcTOTIODQbZsck_4")
+6969597735=os.environ.get("6969597735")
+
+# تأكد إنهم موجودين
+if not 7880550955:AAEep2yo54KzCLXqKHUWcTOTIODQbZsck_4 or not 6969597735:
+    raise ValueError("⚠️ BOT_TOKEN أو CHAT_ID مفقودين من Environment Variables!")
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bot is running!"
+    return "✅ Bot is running!"
 
 @app.route("/send", methods=["POST"])
 def send_message():
     data = request.json
-    text = data.get("text", "No message provided")
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    text = data.get("text", "🚫 لا توجد رسالة")
+
+    url = f"https://api.telegram.org/bot{7880550955:AAEep2yo54KzCLXqKHUWcTOTIODQbZsck_4}/sendMessage"
     payload = {
-        "chat_id": 6969597735,
+        "6969597735": CHAT_ID,
         "text": text
     }
-    r = requests.post(url, json=payload)
-    return str(r.json())
+
+    try:
+        response = requests.post(url, json=payload)
+        print("رد تيليجرام:", response.text)
+        return response.text
+    except Exception as e:
+        print("⚠️ خطأ أثناء إرسال الرسالة:", e)
+        return {"error": str(e)}, 500
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
